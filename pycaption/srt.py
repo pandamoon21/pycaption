3afe28d0998ffdc56cad5c5cba5729729bc090d8
pandamoon21 <33972938+pandamoon21@users.noreply.github.com>
2023-01-12 18:03:33 +0700

    Change nodes

--- pycaption/srt.py
+++ pycaption/srt.py
@@ -110,9 +110,7 @@ class SRTWriter(BaseWriter):
                 merged_captions[-1] = Caption(
                     start=caption.start,
                     end=caption.end,
-                    nodes=(merged_captions[-1].nodes
-                           + [CaptionNode.create_break()]
-                           + caption.nodes))
+                    nodes=(merged_captions[-1].nodes))
             else:
                 # Different timestamp, end of merging, append new caption
                 merged_captions.append(caption)
